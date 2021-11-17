e5485758eb47a1b71dca94c509b841cc9850e0ab
UberUkrop <lesorubes124@gmail.com>
2021-11-17 14:35:28 +0200

    Initial commit

---
+++ CMakeFiles/OpenMP_lab.dir/DependInfo.cmake
@@ -0,0 +1,21 @@
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+set(CMAKE_DEPENDS_CHECK_CXX
+  "B:/OpenMP_lab/main.cpp" "B:/OpenMP_lab/cmake-build-debug/CMakeFiles/OpenMP_lab.dir/main.cpp.obj"
+  "B:/OpenMP_lab/main_without_mp.cpp" "B:/OpenMP_lab/cmake-build-debug/CMakeFiles/OpenMP_lab.dir/main_without_mp.cpp.obj"
+  )
+set(CMAKE_CXX_COMPILER_ID "Clang")
+
+# The include file search paths:
+set(CMAKE_CXX_TARGET_INCLUDE_PATH
+  )
+
+# Targets to which this target links.
+set(CMAKE_TARGET_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ CMakeFiles/mp.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/mp.dir/main.c.obj"
+  "mp.exe"
+  "mp.exe.manifest"
+  "mp.lib"
+  "mp.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/mp.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
